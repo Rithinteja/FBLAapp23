7c986415b34186801873238fd6ca7b5958ab55c8
AnujP75 <97137002+AnujP75@users.noreply.github.com>
2023-03-19 17:54:37 -0400

    Update ImageCopy.js

--- .github/workflows/ImageCopy.js
+++ .github/workflows/ImageCopy.js
@@ -1,36 +1,95 @@
-# This is a basic workflow to help you get started with Actions
+//run npm install expo-image-picker
+//run npm install expo-clipboard
+import React, { useState, useEffect } from "react";
+import { Text, View, Button, Image, Alert, Share } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Clipboard from 'expo-clipboard';
 
-name: CI
+const ShareExample = () => {
+  //Absence Notification
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'Notify School about Absence.',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
 
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the "main" branch
-  push:
-    branches: [ "main" ]
-  pull_request:
-    branches: [ "main" ]
+  //Image Picker
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const[ image, setImage ] = useState(null);
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+  useEffect(() => {
+    (async () => {
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
+    })();
+  }, []);
 
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4,3],
+      quality: 1,
+      base64: true,
+    });
 
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v3
+    console.log(result);
 
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
+    await Clipboard.setImageAsync(result.base64);
 
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+
+  };
+
+  if (hasGalleryPermission === false) {
+    return <Text>No access to Internal Storage</Text>
+  }
+
+  //Sharing Photos
+  const onImageShare = async () => {
+
+    try {
+      const result = await Share.share({
+        message:
+        'Paste image to Share!',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
+
+  return (
+    <View style={{flex:1, justifyContent:"center", backgroundColor: "black"}}>
+      <Button onPress={onShare} title="Absence Notification" />
+      <Button onPress={pickImage} title="Pick Image to Share" />
+      <Button onPress={onImageShare} title="Share Image" />
+    </View>
+  );
+};
+
+export default ShareExample;
